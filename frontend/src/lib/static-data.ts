0c0acb003b0daa203c9d757dc2b21f33170cd2d1
MedamoniSharan <sharan.medamoni1729@gmail.com>
2026-02-20 14:10:15 +0530

    udpated images in hero

--- frontend/src/lib/static-data.ts
+++ frontend/src/lib/static-data.ts
@@ -182,8 +182,8 @@ const HERO_IMAGES_DIR = '/hero_images';
 
 export const staticBannersHero: StaticBanner[] = [
 
-  { _id: 'banner-hero-3', title: 'Anandh Bunk Stores', subtitle: 'Visit Us', image: `${HERO_IMAGES_DIR}/3.png`, link: '/', position: 'hero', order: 1 },
-  { _id: 'banner-hero-4', title: 'Anandh Bunk Stores', subtitle: 'Shop Now', image: `${HERO_IMAGES_DIR}/4.png`, link: '/', position: 'hero', order: 2 },
+  { _id: 'banner-hero-3', title: 'Anandh Bunk Stores', subtitle: 'Visit Us', image: `${HERO_IMAGES_DIR}/5.png`, link: '/', position: 'hero', order: 1 },
+  { _id: 'banner-hero-4', title: 'Anandh Bunk Stores', subtitle: 'Shop Now', image: `${HERO_IMAGES_DIR}/6.png`, link: '/', position: 'hero', order: 2 },
 ];
 
 export const staticBannersMiddle: StaticBanner[] = [
